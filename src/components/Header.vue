2cc7af2faae550e42e0773f85c272fc99bb2ea5f
Delkys Welffer <delkys.yo@gmail.com>
2021-05-15 19:48:30 -0400

    First commit

---
+++ src/components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+    <header>
+        <h1>{{ title }}</h1>
+        <Button text="Add Task" color="blue" />
+    </header>
+</template>
+
+<script>
+import Button from './Button'
+
+export default {
+    name: 'Header',
+    props: {
+        title: {
+            type: String,
+            default: 'A Header'
+        }
+    },
+    components: {
+        Button
+    }
+}
+</script>
+
+<style scoped>
+    header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom: 20px;
+    }
+</style>
