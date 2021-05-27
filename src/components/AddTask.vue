bc5b3513dc7440cd083fc3d4244db2aa8d5ae42b
Delkys Welffer <delkys.yo@gmail.com>
2021-05-26 23:34:27 -0400

    Add Task funcionality

--- src/components/AddTask.vue
+++ src/components/AddTask.vue
@@ -25,6 +25,36 @@
 <script>
 export default {
   name: 'AddTask',
+  data() {
+    return {
+      text: '',
+      day: '',
+      reminder: false,
+    }
+  },
+  methods: {
+    onSubmit(e) {
+      e.preventDefault();
+
+      if(!this.text) {
+        alert("Please add a task!")
+        return
+      }
+
+      const newTask = {
+        id: Math.floor(Math.random() * 100000),
+        text: this.text,
+        day: this.day,
+        reminder: this.reminder
+      }
+      
+      this.$emit('add-task', newTask)
+
+      this.text = ''
+      this.day = ''
+      this.reminder = false
+    }
+  }
 };
 </script>
 
